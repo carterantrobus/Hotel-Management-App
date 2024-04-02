33f0e0beb02590877f8afaa55fb34bea69a98ca3
Carter Antrobus <carter.antrobus@ontariotechu.net>
2024-04-01 22:33:09 -0400

    first commit

---
+++ src/main/java/com/example/hmp/HmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hmp/HmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
